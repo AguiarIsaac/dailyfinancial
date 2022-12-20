b3367742621ab29e0b1be284f21c85fefa60fdb9
AguiarIsaac <isaacaguiar_@hotmail.com>
2022-12-20 17:53:18 -0300

    limpeza

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+export function App() {
+  const [value, setValue] = useState(new Date())
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <Calendar onChange={setValue} value={value} />
   )
 }
 
-export default App
