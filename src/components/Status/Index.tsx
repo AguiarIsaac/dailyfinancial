f45acaf7393ba07b4c0239afe6e3eb38b74db63c
AguiarIsaac <isaacaguiar_@hotmail.com>
2023-01-10 17:39:40 -0300

    init status

--- src/components/Status/Index.tsx
+++ src/components/Status/Index.tsx
@@ -1,3 +1,40 @@
+import { SectionStatus } from "./styles";
+
 export function Status() {
-  return <h1>Status</h1>
+  return (
+    <SectionStatus>
+      <header>
+      <h1>Status financeiro</h1>
+      </header>
+
+      <div className="content">
+        <h2>Mês atual: Janeiro</h2>
+
+        <form>
+          <h3>Selecionar Periodo: </h3>
+          <input type="month" name="" id="" />
+          <button type="submit">Buscar</button>
+        </form>
+
+        <div className="analitcs">
+          <div className="line">
+            <p>Entradas: R$ 2.080,00</p>
+          </div>
+
+          <div className="line">
+            <p>Saídas: R$ 1.080,00</p>
+          </div>
+
+          <div className="line">
+            <p>Aportes: R$ 280,00.</p>
+          </div>
+
+          <div className="line">
+            <p>Dividendos: R$ 0,90.</p>
+          </div>
+
+        </div>
+      </div>
+    </SectionStatus>
+  )
 }
